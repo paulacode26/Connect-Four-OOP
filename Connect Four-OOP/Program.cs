27f73c37fdc7cfc3e0d0d785a41cf95b0289353b
paulacode26 <pgperez26@gmail.com>
2024-03-22 16:34:49 -0600

    PAULA GOMEZ: I created player abstract class and Humanplayer subclass

--- Connect Four-OOP/Program.cs
+++ Connect Four-OOP/Program.cs
@@ -8,8 +8,47 @@ namespace Connect_Four_OOP
 {
     internal class Program
     {
+
+        //creating player abstract class:
+        public abstract class player
+        {
+            public string Name;
+            public char Symbole;
+
+            public player(string name, char symbol)
+            {
+                Name = name;
+                Symbole = symbol;
+            }
+
+            public override string ToString()
+            {
+                return $"Name: {Name}, Symbole: {Symbole}";
+            }
+        }
+        // Subclass of the abstract class:
+        public class Humanplayer : player
+        {
+            public Humanplayer(string name, char symbol) : base(name, symbol)
+            {
+
+            }
+
+            public override string ToString()
+            {
+                return base.ToString(); // use the inf set on the base class.
+            }
+        }
+
         static void Main(string[] args)
         {
+            // Creating instances of the subclasses( base class player)
+            Humanplayer player1 = new Humanplayer("Player X", 'X');
+            Humanplayer player2 = new Humanplayer("Player O", 'O');
+
+            Console.WriteLine(player1);
+            Console.WriteLine(player2);
+
         }
     }
 }
